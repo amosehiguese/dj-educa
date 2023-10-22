b5f8d60239dc8a82afaf875ad45f6546ce911265
amosehiguese <amosehiguese@gmail.com>
2023-10-22 14:56:01 +0100

    Implement restful apis

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,6 +8,8 @@ from rest_framework import generics
 from rest_framework.decorators import action
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -44,3 +46,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
